cf513dcdbf897de7c25b45c8137bd0107080e8f6
pierss <bonkbonksourman@gmail.com>
2023-01-03 16:59:08 +0100

    Added button styling

---
+++ src/constants/supportedWalletProviders.ts
@@ -0,0 +1,57 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { AbstractConnector } from '@web3-react/abstract-connector';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+
+import metamaskLogo from '../images/metamask.svg';
+import walletConnectLogo from '../images/walletconnect.svg';
+
+declare let window: any;
+
+export type WalletProvider = {
+  name: string;
+  logo: string;
+  isInstalled: boolean;
+  url: string;
+  getConnector: () => AbstractConnector;
+};
+
+const cachedConnectors: Record<string, AbstractConnector> = {};
+
+const SUPPORTED_WALLET_PROVIDERS: WalletProvider[] = [
+  {
+    name: 'MetaMask',
+    logo: metamaskLogo,
+    isInstalled: typeof window.ethereum !== 'undefined',
+    url: 'https://metamask.io/',
+    getConnector: () => {
+      if (!cachedConnectors.MetaMask) {
+        cachedConnectors.MetaMask = new InjectedConnector({
+          supportedChainIds: [1, 4, 5, 56, 97, 137, 43113, 43114, 80001],
+        });
+      }
+      return cachedConnectors.MetaMask;
+    },
+  },
+  {
+    name: 'WalletConnect',
+    logo: walletConnectLogo,
+    isInstalled: true,
+    url: 'https://walletconnect.com/',
+    getConnector: () => {
+      if (!cachedConnectors.WalletConnect) {
+        cachedConnectors.WalletConnect = new WalletConnectConnector({
+          rpc: {
+            1: process.env.REACT_APP_RPC_URL_1 || '',
+            5: process.env.REACT_APP_RPC_URL_5 || '',
+          },
+        });
+      }
+      return cachedConnectors.WalletConnect;
+    },
+  },
+];
+
+export default SUPPORTED_WALLET_PROVIDERS;
+
+export { AbstractConnector };
