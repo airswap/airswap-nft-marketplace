2e4299f19a728f08674573dede0dffbfcbd2e607
pierss <bonkbonksourman@gmail.com>
2023-01-12 22:55:40 +0100

    27: Added caching to WalletConnector

--- src/compositions/Page/Page.tsx
+++ src/compositions/Page/Page.tsx
@@ -2,9 +2,11 @@ import React, { FC, ReactNode, useState } from 'react';
 
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
+import classNames from 'classnames';
 
 import Button from '../../components/Button/Button';
 import useToggle from '../../hooks/useToggle';
+import { useAppSelector } from '../../redux/hooks';
 import WalletConnector from '../../widgets/WalletConnector/WalletConnector';
 import MobileMenu from '../MobileMenu/MobileMenu';
 import TopBar from '../TopBar/TopBar';
@@ -19,19 +21,24 @@ interface PageProps {
 
 const Page: FC<PageProps> = ({ className = '', contentClassName = '', children }) => {
   const { active } = useWeb3React<Web3Provider>();
+  const { isInitialized } = useAppSelector((state) => state.web3);
 
   const [mobileMenuIsVisible, setMobileMenuIsVisible] = useState<boolean>(false);
   const [showWalletConnector, toggleShowWalletConnector] = useToggle(!active);
+
+  const pageClassName = classNames('page', {
+    'page--show-wallet-connector': showWalletConnector && isInitialized && !active,
+  }, className);
 
   const handleIconButtonClick = (): void => {
     setMobileMenuIsVisible(!mobileMenuIsVisible);
   };
 
   return (
-    <div className={`page ${className}`}>
+    <div className={pageClassName}>
       <TopBar
         mobileMenuIsVisible={mobileMenuIsVisible}
-        showDesktopConnectButton={!active}
+        showDesktopConnectButton={isInitialized && !active}
         onConnectButtonClick={toggleShowWalletConnector}
         onMobileMenuButtonClick={handleIconButtonClick}
         className="page__top-bar"
@@ -42,12 +49,10 @@ const Page: FC<PageProps> = ({ className = '', contentClassName = '', children }
         onNavLinkClick={handleIconButtonClick}
         className="page__mobile-menu"
       />
-      {(!active && showWalletConnector) && (
-        <WalletConnector
-          className="page__wallet-connector"
-          onCloseButtonClick={toggleShowWalletConnector}
-        />
-      )}
+      <WalletConnector
+        onCloseButtonClick={toggleShowWalletConnector}
+        className="page__wallet-connector"
+      />
 
       <div className={`page__content ${contentClassName}`}>
         {children}
--- src/widgets/WalletConnector/WalletConnector.tsx
+++ src/widgets/WalletConnector/WalletConnector.tsx
@@ -1,10 +1,13 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
 import { Web3Provider } from '@ethersproject/providers';
 import { useWeb3React } from '@web3-react/core';
 
 import IconButton from '../../compositions/IconButton/IconButton';
 import SUPPORTED_WALLET_PROVIDERS, { WalletProvider } from '../../constants/supportedWalletProviders';
+import { useAppDispatch } from '../../redux/hooks';
+import { getLastProviderFromLocalStorage } from '../../redux/stores/web3/web3Api';
+import { setIsInitialized, setWalletName } from '../../redux/stores/web3/web3Slice';
 import WalletProviderList from './subcomponents/WalletProviderList/WalletProviderList';
 
 import './WalletConnector.scss';
@@ -15,10 +18,27 @@ interface WalletConnectorProps {
 }
 
 const WalletConnector: FC<WalletConnectorProps> = ({ onCloseButtonClick, className = '' }) => {
+  const dispatch = useAppDispatch();
+
   const { activate } = useWeb3React<Web3Provider>();
 
+  const activateWallet = async (provider: WalletProvider) => {
+    dispatch(setWalletName(provider.name));
+    await activate(provider.getConnector());
+    dispatch(setIsInitialized(true));
+  };
+
+  useEffect(() => {
+    const provider = getLastProviderFromLocalStorage();
+    if (provider) {
+      activateWallet(provider);
+    } else {
+      dispatch(setIsInitialized(true));
+    }
+  }, []);
+
   const handleWalletProviderButtonClick = (provider: WalletProvider): void => {
-    activate(provider.getConnector());
+    activateWallet(provider);
   };
 
   return (
