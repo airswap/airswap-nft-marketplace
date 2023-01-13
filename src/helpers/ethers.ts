3459780093a0cd379d1249f90f1d83ab32b30f33
Goddak <kris@goddak.co.uk>
2023-01-13 16:54:54 +0000

    useNFTMetadata Hook

--- src/helpers/ethers.ts
+++ src/helpers/ethers.ts
@@ -13,3 +13,6 @@ export const setLibrary = (provider: any): Web3Provider => {
 };
 
 export const getLibrary = (chainId: number): Web3Provider => cachedLibrary[chainId];
+
+export const startsWithIPFS: (str: string) => boolean = (str: string) => str.startsWith('ipfs://');
+export const ipfsToUrl: (string: string) => string = (ipfsAddress) => `${process.env.REACT_APP_IPFS_GATEWAY_URL}${ipfsAddress.split('ipfs://')[1]}`;
--- src/widgets/CollectionWidget/CollectionWidget.tsx
+++ src/widgets/CollectionWidget/CollectionWidget.tsx
@@ -1,12 +1,19 @@
-import React, { FC } from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { FC, useEffect } from 'react';
 
+import useNftMetadata from '../../hooks/useNftMetadata';
 import { useAppSelector } from '../../redux/hooks';
 import CollectionPortrait from './subcomponents/CollectionPortrait/CollectionPortrait';
 
 import './CollectionWidget.scss';
 
 const CollectionWidget: FC = () => {
-  const { collectionImage, collectionName } = useAppSelector((state) => state.config);
+  const { collectionImage, collectionName, collectionToken } = useAppSelector(
+    (state) => state.config,
+  );
+
+  const nftMetadata = useNftMetadata(collectionToken, '3060');
 
   return (
     <div className="collection-widget">
