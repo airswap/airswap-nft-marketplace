452ee5b93bd0af491552717bd065794818eff361
Filip Manole <filip.manole@gmail.com>
2023-01-17 18:42:48 +0200

    added classname to the component props

--- src/components/NFTCard/NFTCard.tsx
+++ src/components/NFTCard/NFTCard.tsx
@@ -6,10 +6,13 @@ type NFTCardProps = {
   imageURI: string;
   name: string;
   price: number;
+  className?: string;
 }
 
-const NFTCard = ({ imageURI, name, price }: NFTCardProps) => (
-  <div className="nft-card">
+const NFTCard = ({
+  imageURI, name, price, className,
+}: NFTCardProps) => (
+  <div className={`nft-card ${className}`}>
     <img className="nft-card__img" src={imageURI} alt={name} />
     <div className="nft-card__name">{name}</div>
     <div className="nft-card__price">{`${price} ETH`}</div>
