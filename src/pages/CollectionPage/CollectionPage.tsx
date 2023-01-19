b98e928c3702fb949ced758f4ec1aa1f4eda377c
Filip Manole <filip.manole@gmail.com>
2023-01-19 15:22:32 +0200

    moved nfts in collection widget

--- src/pages/CollectionPage/CollectionPage.tsx
+++ src/pages/CollectionPage/CollectionPage.tsx
@@ -1,55 +1,17 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 
-import NFTCard from '../../components/NFTCard/NFTCard';
 import Page from '../../compositions/Page/Page';
-import useInfiniteScroll from '../../hooks/useInfiniteScroll';
-import { useAppDispatch, useAppSelector } from '../../redux/hooks';
-import { fetchNFTMetadata } from '../../redux/stores/collection/collectionApi';
 import CollectionWidget from '../../widgets/CollectionWidget/CollectionWidget';
 
 import './CollectionPage.scss';
 
-const CollectionPage: FC = () => {
-  const { isActive } = useAppSelector((state) => state.web3);
-  const { tokensData } = useAppSelector((state) => state.collection);
-  const dispatch = useAppDispatch();
-
-  const fetchCallback = async (): Promise<void> => {
-    await dispatch(fetchNFTMetadata());
-  };
-
-  const [error] = useState<boolean>(false);
-
-  const shouldNotDisplay = !isActive || error;
-
-
-  const { isLoading } = useInfiniteScroll({ fetchCallback });
-
-  return (
-    <Page
-      className="collection-page"
-      contentClassName="collection-page__content"
-    >
-      {shouldNotDisplay
-        ? <div className="status">Collection could not be displayed...</div>
-        : (
-          <>
-            <CollectionWidget />
-            <div className="nft-list">
-              {tokensData.map((t) => (
-                <NFTCard
-                  key={t.name}
-                  name={t.name}
-                  imageURI={t.image.replace('ipfs://', 'https://ipfs.io/ipfs/')}
-                  price={Math.floor(Math.random() * (1000 - 100) + 100) / 100}
-                />
-              ))}
-            </div>
-            {isLoading && <div className="loading">Fetching more NFTs ...</div>}
-          </>
-      )}
-    </Page>
-  );
-};
+const CollectionPage: FC = () => (
+  <Page
+    className="collection-page"
+    contentClassName="collection-page__content"
+  >
+    <CollectionWidget />
+  </Page>
+);
 
 export default CollectionPage;
--- src/widgets/CollectionWidget/CollectionWidget.tsx
+++ src/widgets/CollectionWidget/CollectionWidget.tsx
@@ -1,12 +1,30 @@
 import React, { FC } from 'react';
 
-import { useAppSelector } from '../../redux/hooks';
+import NFTCard from '../../components/NFTCard/NFTCard';
+import useInfiniteScroll from '../../hooks/useInfiniteScroll';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { fetchNFTMetadata } from '../../redux/stores/collection/collectionApi';
 import CollectionPortrait from './subcomponents/CollectionPortrait/CollectionPortrait';
 
 import './CollectionWidget.scss';
 
 const CollectionWidget: FC = () => {
   const { collectionImage, collectionName } = useAppSelector((state) => state.config);
+  const { isActive } = useAppSelector((state) => state.web3);
+  const { tokensData } = useAppSelector((state) => state.collection);
+
+  const dispatch = useAppDispatch();
+  const fetchCallback = async (): Promise<void> => {
+    await dispatch(fetchNFTMetadata());
+  };
+
+  const { isLoading } = useInfiniteScroll({ fetchCallback });
+
+  if (!isActive) {
+    return (
+      <div className="status">Collection could not be displayed...</div>
+    );
+  }
 
   return (
     <div className="collection-widget">
@@ -16,6 +34,17 @@ const CollectionWidget: FC = () => {
         title={collectionName}
         className="collection-widget__portrait"
       />
+      <div className="nft-list">
+        {tokensData.map((t) => (
+          <NFTCard
+            key={t.name}
+            name={t.name}
+            imageURI={t.image.replace('ipfs://', 'https://ipfs.io/ipfs/')}
+            price={Math.floor(Math.random() * (1000 - 100) + 100) / 100}
+          />
+        ))}
+      </div>
+      {isLoading && <div className="loading">Fetching more NFTs ...</div>}
     </div>
   );
 };
