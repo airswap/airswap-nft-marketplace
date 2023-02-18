9bdeebb33035053d9755777afb9f802d9707b15d
GiuntaLucas <lucas-giunta@outlook.com>
2023-02-18 18:12:30 +0100

    WalletInfo integration

--- src/widgets/ProfileWidget/ProfileWidget.tsx
+++ src/widgets/ProfileWidget/ProfileWidget.tsx
@@ -1,7 +1,24 @@
 import React, { FC } from 'react';
 
+import { Web3Provider } from '@ethersproject/providers';
+import { useWeb3React } from '@web3-react/core';
+
+import useEnsAddress from '../../hooks/useEnsAddress';
+import { useAppSelector } from '../../redux/hooks';
 import ProfileHeader from './subcomponents/ProfileHeader/ProfileHeader';
 
-const ProfileWidget: FC = () => <ProfileHeader />;
+import './ProfileWidget.scss';
+
+const ProfileWidget: FC = () => {
+  const { account } = useWeb3React<Web3Provider>();
+  const ensAddress = useEnsAddress(account || '');
+  const { avatarUrl } = useAppSelector((state) => state.user);
+
+  return (
+    <div className="profile-widget">
+      <ProfileHeader avatarUrl={avatarUrl} ensAddress={ensAddress} address={account || ''} />
+    </div>
+  );
+};
 
 export default ProfileWidget;
--- src/widgets/ProfileWidget/subcomponents/ProfileHeader/ProfileHeader.tsx
+++ src/widgets/ProfileWidget/subcomponents/ProfileHeader/ProfileHeader.tsx
@@ -1,8 +1,31 @@
 import React, { FC } from 'react';
 
+import WalletInfo from '../../../../components/WalletInfo/WalletInfo';
+
 import './ProfileHeader.scss';
 
+interface ProfileHeaderProp {
+  avatarUrl?: string;
+  address?: string;
+  ensAddress?: string;
+  className?: string;
+}
 
-const ProfileHeader: FC = () => <h1>profile header</h1>;
+const ProfileHeader: FC<ProfileHeaderProp> = ({
+  avatarUrl,
+  ensAddress,
+  address,
+  className = '',
+}) => {
+  const handleDisconnectClick = () => {
+    console.log('handleDisconnectClick');
+  };
+  return (
+    <div className={className}>
+      <WalletInfo isBanner avatarUrl={avatarUrl} ensAddress={ensAddress} address={address} onLogoutButtonClick={handleDisconnectClick} />
+    </div>
+  );
+};
+
 
 export default ProfileHeader;
