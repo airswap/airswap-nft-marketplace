17fb767766eb6e00669dc8f655ab3c193f98091b
Filip Manole <filip.manole@gmail.com>
2023-02-27 22:10:49 +0200

    package.json and script update

--- getTokenIds.js
+++ getTokenIds.js
@@ -1,22 +1,26 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require('fs');
-const { ADDRESS_ZERO, tokenKinds } = require('@airswap/constants');
 const ethers = require('ethers');
+const dotenv = require('dotenv');
+
+const { ADDRESS_ZERO, tokenKinds } = require('@airswap/constants');
 
 const { abi: ERC165_ABI } = require('@openzeppelin/contracts/build/contracts/ERC165.json');
 const { abi: ERC721_ABI } = require('@openzeppelin/contracts/build/contracts/ERC721.json');
 const { abi: ERC1155_ABI } = require('@openzeppelin/contracts/build/contracts/ERC1155.json');
 
-const address = process.argv[2];
-const network = process.argv[3];
-const apikey = process.argv[4];
+/* load env variables */
+dotenv.config();
 
-/* sanity check */
-if (!address || !network) console.log("Incorrect arguments...");
+const address = process.env.REACT_APP_COLLECTION_TOKEN;
+const network = ethers.providers.getNetwork(process.env.REACT_APP_CHAIN_ID);
+const apikey = process.env.REACT_APP_INFURA_API_KEY;
+
+/* check address */
 if (!ethers.utils.isAddress(address)) throw new Error(`Invalid address: ${address}`);
 
 /* initialization */
-let provider = !apikey ? ethers.getDefaultProvider(network) : ethers.providers.InfuraProvider.getWebSocketProvider(network, apikey);
+const provider = !apikey ? ethers.getDefaultProvider(network) : ethers.providers.InfuraProvider.getWebSocketProvider(network, apikey);
 const contract = new ethers.Contract(address, ERC165_ABI, provider);
 
 /* if contract is ERC721 */
@@ -31,9 +35,7 @@ contract.supportsInterface(tokenKinds.ERC721).then((isERC721) => {
     const tokenIds = events.map(e => e.args?.at(2).toString());
 
     /* get unique values */
-    const uniqueTokenIds = tokenIds.filter((element, index) => {
-      return tokenIds.indexOf(element) === index;
-    });
+    const uniqueTokenIds = tokenIds.filter((element, index) => tokenIds.indexOf(element) === index);
 
     /* write token ids in a json file */
     fs.writeFileSync('src/constants/tokenIds.json', JSON.stringify(uniqueTokenIds));
@@ -54,9 +56,7 @@ contract.supportsInterface(tokenKinds.ERC1155).then((isERC1155) => {
     const tokenIds = events.map(e => e.args?.at(3).toString());
 
     /* get unique values */
-    const uniqueTokenIds = tokenIds.filter((element, index) => {
-      return tokenIds.indexOf(element) === index;
-    });
+    const uniqueTokenIds = tokenIds.filter((element, index) => tokenIds.indexOf(element) === index);
 
     /* write token ids in a json file */
     fs.writeFileSync('src/constants/tokenIds.json', JSON.stringify(uniqueTokenIds));
