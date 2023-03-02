902e9c9ef4bd24a38e3cda5541f753f48838389c
GiuntaLucas <lucas-giunta@outlook.com>
2023-03-02 14:11:03 +0100

    padding issue

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -3,8 +3,13 @@ import React, { FC } from 'react';
 import Page from '../../compositions/Page/Page';
 import ProfileWidget from '../../widgets/ProfileWidget/ProfileWidget';
 
+import './ProfilePage.scss';
+
 const ProfilePage: FC = () => (
-  <Page>
+  <Page
+    className="profile-page"
+    contentClassName="profile-page__content"
+  >
     <ProfileWidget />
   </Page>
 );
