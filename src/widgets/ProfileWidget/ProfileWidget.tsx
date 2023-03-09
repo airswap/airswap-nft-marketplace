257cebf62b2952194ff5ca72ba59a0cae220c106
Goddak <kris@goddak.co.uk>
2023-03-09 08:45:27 +0000

    Initial commit

---
+++ src/widgets/ProfileWidget/ProfileWidget.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+
+import './ProfileWidget.scss';
+
+const ProfileWidget: FC = () => {
+  console.log('ProfileWidget');
+  return <div className="profile-widget">ProfileWidget</div>;
+};
+
+export default ProfileWidget;
