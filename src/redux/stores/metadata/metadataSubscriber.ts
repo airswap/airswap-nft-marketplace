938a4d5ab954e0f4c4ced5aeac7401862b4d1a4c
pierss <bonkbonksourman@gmail.com>
2023-04-13 08:41:05 +0200

    progress

--- src/redux/stores/metadata/metadataSubscriber.ts
+++ src/redux/stores/metadata/metadataSubscriber.ts
@@ -1,7 +1,7 @@
 import { getLibrary } from '../../../helpers/ethers';
 import { store } from '../../store';
 import { fetchProtocolFee } from './metadataActions';
-import { getCurrencyAndCollectionTokenInfo } from './metadataApi';
+import { getCurrencyTokenInfo } from './metadataApi';
 
 export const configureMetadataSubscriber = () => {
   let initialized = false;
@@ -14,7 +14,7 @@ export const configureMetadataSubscriber = () => {
 
       const library = getLibrary(web3.chainId);
 
-      store.dispatch(getCurrencyAndCollectionTokenInfo({
+      store.dispatch(getCurrencyTokenInfo({
         currencyToken: config.currencyToken,
         collectionToken: config.collectionToken,
         library,
