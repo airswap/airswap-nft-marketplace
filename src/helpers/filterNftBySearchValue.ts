9a8bc84d9fb115607dbfa901a7472ff0452a0807
Kris Goddard <Goddak@users.noreply.github.com>
2023-05-30 14:36:45 +0100

    Update src/helpers/filterNftBySearchValue.ts
    Co-authored-by: piersss <86911296+piersss@users.noreply.github.com>

--- src/helpers/filterNftBySearchValue.ts
+++ src/helpers/filterNftBySearchValue.ts
@@ -1,25 +1,13 @@
 import { CollectionTokenInfo } from '@airswap/types';
 
-/**
- * Filter NFT by search value.
- * @param sValue The search value.
- * @param nft The NFT to filter.
- * @returns true if the NFT should be kept, false otherwise.
- */
-const filterNftBySearchValue = (sValue: string, nft: CollectionTokenInfo) => {
-  // If the search query is empty keep the nft.
-  if (sValue === '') return true;
-  // We can search by id, name, description & attribute values.
-  if (nft.name && nft.name.toLowerCase().includes(sValue.toLowerCase())) return true;
-  if (nft.description && nft.description.toLowerCase().includes(sValue.toLowerCase())) return true;
-  if (nft.id.toString().toLowerCase().includes(sValue.toLowerCase())) return true;
-  if (nft.attributes) {
-    for (let j = 0; j < nft.attributes.length; j += 1) {
-      const attribute = nft.attributes[j];
-      if (attribute.value.toString().toLowerCase().includes(sValue.toLowerCase())) return true;
-    }
-  }
-  return false;
+import { CollectionTokenInfo } from '@airswap/types';
+
+const filterNftBySearchValue = (value: string, nft: CollectionTokenInfo): boolean => {
+  if (value === '') return true;
+
+  if (nft.name && nft.name.toLowerCase().includes(value.toLowerCase())) return true;
+
+  return nft.id.toString().toLowerCase().includes(value.toLowerCase());
 };
 
 export default filterNftBySearchValue;
