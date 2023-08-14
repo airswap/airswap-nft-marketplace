90ded1ad2d6ecba0d7b3238fd40ec9f20c8f0d77
IssyPro101 <israeladelaja@gmail.com>
2023-08-14 22:20:09 +1000

    etherscan owned tokens by account url fixed

--- src/helpers/airswap/getOwnedTokensByAccountUrl.ts
+++ src/helpers/airswap/getOwnedTokensByAccountUrl.ts
@@ -4,7 +4,7 @@ const getOwnedTokensByAccountUrl = (chainId: number, account: string, contract: 
   const accountUrl = getAccountUrl(chainId, account);
   const [etherScanUrl] = accountUrl.split('/address/');
 
-  return `${etherScanUrl}/token/${contract}?a=${account}`;
+  return `${etherScanUrl}/tokenholdings/${contract}?a=${account}`;
 };
 
 export default getOwnedTokensByAccountUrl;
