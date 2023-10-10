0ba83e8f576d443ed48b5550e8500bf066b0afe8
piersss <86911296+piersss@users.noreply.github.com>
2023-10-10 12:22:47 +0200

    153: Added excludeNonces to getCollectionOrders (#154)

--- src/redux/stores/collection/collectionApi.ts
+++ src/redux/stores/collection/collectionApi.ts
@@ -1,6 +1,10 @@
 import { FullOrder, OrderFilter } from '@airswap/types';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
+import {
+  isCancelOrderTransaction,
+  isOrderTransaction,
+} from '../../../entities/SubmittedTransaction/SubmittedTransactionHelpers';
 import { AppThunkApiConfig } from '../../store';
 import { getOrdersFromIndexers } from '../indexer/indexerHelpers';
 import { addGetOrderFailedToast } from '../toasts/toastsActions';
@@ -11,15 +15,20 @@ FullOrder[],
 Pick<OrderFilter, 'limit' | 'offset' | 'excludeNonces'>,
 AppThunkApiConfig
 >('collection/getCollectionOrders', async (filter, { dispatch, getState }) => {
-  const { config, indexer } = getState();
+  const { config, indexer, transactions: transactionsState } = getState();
 
   const { collectionToken, currencyToken } = config;
+  const { transactions } = transactionsState;
+  const orderTransactions = transactions.filter(isOrderTransaction);
+  const cancelTransactions = transactions.filter(isCancelOrderTransaction);
+  const excludeNonces = [...orderTransactions, ...cancelTransactions].map(transaction => transaction.order.nonce);
 
   dispatch(setOffset(filter.limit + filter.offset));
 
   try {
     return await getOrdersFromIndexers({
       ...filter,
+      excludeNonces,
       signerTokens: [collectionToken],
       senderTokens: [currencyToken],
     }, indexer.urls);
