a3cc8c95ca1ee500aef6c7603c23d88484945d1f
piersss <86911296+piersss@users.noreply.github.com>
2023-12-02 09:19:54 +0100

    144: Added coinbase to wallet providers (#167)

---
+++ src/web3-connectors/coinbase.ts
@@ -0,0 +1,32 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { CoinbaseWallet } from '@web3-react/coinbase-wallet';
+import { initializeConnector } from '@web3-react/core';
+
+import { rpcUrls } from '../constants/rpc';
+import { Connection, ConnectionType } from './connections';
+import { onConnectionError } from './helpers';
+
+const chainId = process.env.REACT_APP_CHAIN_ID || '1';
+const rpcUrl = chainId ? rpcUrls[Number(chainId)] : process.env.REACT_APP_RPC_URL_1;
+
+export function buildCoinbaseWalletConnector() {
+  // @ts-ignore
+  const [web3CoinbaseWallet, web3CoinbaseWalletHooks] = initializeConnector<CoinbaseWallet>(
+    (actions) => new CoinbaseWallet({
+      actions,
+      options: {
+        appName: `${process.env.REACT_APP_COLLECTION_NAME} Marketplace`,
+        url: rpcUrl || '',
+        overrideIsCoinbaseWallet: true,
+      },
+      onError: onConnectionError,
+    }),
+  );
+  const coinbaseWalletConnection: Connection = {
+    connector: web3CoinbaseWallet,
+    hooks: web3CoinbaseWalletHooks,
+    type: ConnectionType.coinbase,
+  };
+
+  return coinbaseWalletConnection;
+}
