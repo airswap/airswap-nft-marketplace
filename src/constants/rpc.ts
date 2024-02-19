5860e9b30c6c6b4324d7a80bb78d2f49d37d2749
pierss <bonkbonksourman@gmail.com>
2024-02-19 22:55:46 +0100

    Optimised code

--- src/constants/rpc.ts
+++ src/constants/rpc.ts
@@ -1,7 +1,7 @@
 import { apiUrls, mainnets, testnets } from '@airswap/utils';
 
-export const rpcUrls: Record<number, string> = {};
-mainnets.concat(testnets).forEach((chainId) => {
-  rpcUrls[chainId] = process.env[`REACT_APP_RPC_URL_${chainId}`] || apiUrls[chainId];
-});
-
+export const rpcUrls: Record<number, string> = [...mainnets, ...testnets]
+  .reduce((acc, chainId) => ({
+    ...acc,
+    [chainId]: process.env[`REACT_APP_RPC_URL_${chainId}`] || apiUrls[chainId],
+  }), {});
