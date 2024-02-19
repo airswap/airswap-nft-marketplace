417e3757e55f21de86646ddcb94aa72673fe5aef
don mosites <mosites@gmail.com>
2024-02-19 23:03:25 +0100

    use default rpc urls with env var override (#213)

--- src/constants/rpc.ts
+++ src/constants/rpc.ts
@@ -1,10 +1,7 @@
-import { ChainIds } from '@airswap/utils';
+import { apiUrls, mainnets, testnets } from '@airswap/utils';
 
-import { SupportedChain } from './supportedChains';
-
-export const rpcUrls: Record<SupportedChain, string | undefined> = {
-  [ChainIds.MAINNET]: process.env.REACT_APP_RPC_URL_1,
-  [ChainIds.POLYGON]: process.env.REACT_APP_RPC_URL_137,
-  [ChainIds.MUMBAI]: process.env.REACT_APP_RPC_URL_80001,
-  [ChainIds.SEPOLIA]: process.env.REACT_APP_RPC_URL_11155111,
-};
+export const rpcUrls: Record<number, string> = [...mainnets, ...testnets]
+  .reduce((acc, chainId) => ({
+    ...acc,
+    [chainId]: process.env[`REACT_APP_RPC_URL_${chainId}`] || apiUrls[chainId],
+  }), {});
